360b6d3678f88b603ce64ca3821f26ec3d9e75f6
Romadlon Rahmatulloh <romadlon711@gmail.com>
2020-06-08 22:07:18 +0700

    Updates

---
+++ precache-manifest.717651296ae5176ffa29e070f845eb09.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "299b487679c7ec6e93d945377db7a168",
+    "url": "/simple-warehouse-management/index.html"
+  },
+  {
+    "revision": "d1c819ea6617d1e4f567",
+    "url": "/simple-warehouse-management/static/css/2.6522a1ab.chunk.css"
+  },
+  {
+    "revision": "d1c819ea6617d1e4f567",
+    "url": "/simple-warehouse-management/static/js/2.dae7e097.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/simple-warehouse-management/static/js/2.dae7e097.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3e9cddee2db0719592b",
+    "url": "/simple-warehouse-management/static/js/main.ae49a588.chunk.js"
+  },
+  {
+    "revision": "c053df80c4cc1694298c",
+    "url": "/simple-warehouse-management/static/js/runtime-main.c06c5247.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/simple-warehouse-management/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/simple-warehouse-management/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/simple-warehouse-management/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/simple-warehouse-management/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/simple-warehouse-management/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/simple-warehouse-management/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/simple-warehouse-management/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/simple-warehouse-management/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/simple-warehouse-management/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/simple-warehouse-management/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/simple-warehouse-management/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/simple-warehouse-management/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/simple-warehouse-management/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/simple-warehouse-management/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/simple-warehouse-management/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/simple-warehouse-management/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
